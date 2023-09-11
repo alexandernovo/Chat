ca5347cde25e8d5fec27928c8fd0c2a0768c7c03
Alexander <alexandernovo84@gmail.com>
2023-09-11 21:40:59 +0800

    fix deployment

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Profile from './components/Profile'
 
 const App = () => {
   return (
-    <Router basename="/Chat">
+    <Router>
       <Routes>
         <Route element={<PrivateRoutes />}>
           <Route element={<Home />} path='/home' ></Route>
