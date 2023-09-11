751270c8d81732fcd244f76e35a64b3f9cc58f02
Alexander <alexandernovo84@gmail.com>
2023-09-11 22:23:41 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://chat-server-cjy8.onrender.com',
         changeOrigin: true,
         secure: false,
         ws: true,
