cb18a7d28c43d3e185d11aeb507a58f225af1831
Alexander <alexandernovo84@gmail.com>
2023-09-12 15:28:38 +0800

    fix contact image

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Avatar1 from '../assets/profile/avatar1.jpg';
+import Placeholder from '../assets/profile/placholder.jpg';
 
 const Contact = ({ contact, onClick }) => {
     return (
@@ -7,7 +7,7 @@ const Contact = ({ contact, onClick }) => {
             {contact && contact.length !== 0 ? (contact.map(result => (
                 <div key={result._id} className='mb-3 text-white cursor-pointer' onClick={() => onClick(result._id)}>
                     <div className='flex items-center py-1'>
-                        <img src={Avatar1} className='h-11 w-11 rounded-full' />
+                        <img src={contact.image ? contact.image : Placeholder} className='h-11 w-11 rounded-full' />
                         <div className='ms-2'>
                             <h6 className='text-[14px]'>{contact ? (`${result.firstname} ${result.lastname}`) : ''}</h6>
                         </div>
